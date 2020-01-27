844acdacd5e1c7f4c8214d6b69c650a4c70a6f9e
luuky50 <luukhulshof@hotmail.nl>
2020-01-27 11:21:10 +0100

    Added PlayerManager

---
+++ C&C Handel Application/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    float horizontal;
+    float vertical;
+
+    [SerializeField]
+    Transform container;
+
+    [SerializeField]
+    float turnSpeedMouse;
+
+    [SerializeField]
+    private GameObject questionObject;
+
+
+    void LateUpdate()
+    {
+        horizontal = Input.GetAxis("Mouse X");
+        vertical = Input.GetAxis("Mouse Y");
+
+        if (Input.GetMouseButton(0))
+        {
+            container.Rotate(new Vector3(0, horizontal * (1), 0f) * Time.deltaTime * turnSpeedMouse);
+            transform.Rotate(new Vector3(vertical, 0, 0) * Time.deltaTime * turnSpeedMouse);
+        }
+    }
+
+    private void PlaceObject()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Instantiate(questionObject, new Vector3(Camera.current.pixelRect.center, 9, 9) )
+
+        }
+
+    }
+
+
+
+
+}
