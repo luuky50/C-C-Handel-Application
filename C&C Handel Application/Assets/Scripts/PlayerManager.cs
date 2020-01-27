537a629ca7cb499fb2adeecf5f8625374b40f804
luuky50 <luukhulshof@hotmail.nl>
2020-01-27 10:20:16 +0100

    Changed Scene Name and added mouse 360 movement

---
+++ C&C Handel Application/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+
+
+    float horizontal;
+    float vertical;
+
+    [SerializeField]
+    Transform container;
+
+    [SerializeField]
+    float turnSpeedMouse;
+
+
+    void LateUpdate()
+    {
+        horizontal = Input.GetAxis("Mouse X");
+        vertical = Input.GetAxis("Mouse Y");
+
+        if (Input.GetMouseButton(0))
+        {
+            container.Rotate(new Vector3(0, horizontal * (1), 0f) * Time.deltaTime * turnSpeedMouse);
+            transform.Rotate(new Vector3(vertical, 0, 0) * Time.deltaTime * turnSpeedMouse);
+        }
+    }
+
+    private void PlaceObject()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Instantiate()
+
+        }
+
+    }
+
+    
+
+
+}
