6c75f6294b3512c7008bf39a39e03bb4a6bfff96
luuky50 <luukhulshof@hotmail.nl>
2020-02-04 20:43:30 +0100

    Added Leentwean editor and panelmanager

---
+++ C&C Handel Application/Assets/Scripts/Classes/Panel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public enum Orientation {Portrait, Landscape, None}
+
+[System.Serializable]
+public class Panel
+{
+    public Transform background;
+
+    public void enable(bool active, bool switchBackground = false)
+    {
+        if(switchBackground)
+            background.gameObject.SetActive(active);
+    }
+}
+
---
+++ C&C Handel Application/Assets/Scripts/PanelManager.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour
+{
+    public static PanelManager instance;
+    public List<Panel> panels;
+
+    private Panel thisPanel;
+    private List<Panel> history;
+
+    void Start()
+    {
+        instance = this;
+        history = new List<Panel>();
+
+        //close all panels
+        foreach (Panel p in panels)
+        {
+            p.enable(false, true);
+        }
+
+        //open first panel
+        thisPanel = panels[1];
+        switchPanel(thisPanel);
+
+        if (thisPanel.background.gameObject != null)
+            thisPanel.background.gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void nextPanel()
+    {
+        //check what the next panel is gonna be
+        if (panels.Count > panels.IndexOf(thisPanel))
+        {
+            switchPanel(panels[panels.IndexOf(thisPanel) + 1]);
+        }
+        else
+        {
+            Debug.Log("We can't go a panel further");
+        }
+    }
+
+    public void previous()
+    {
+        if (history.Count > 1)
+        {
+            //remove last panel
+            history.Remove(history[history.Count - 1]);
+            //switch to previous panel
+            switchPanel(history[history.Count - 1], false);
+        }
+
+    }
+
+    public void goToPanel(Transform panelToSwitchTo)
+    {
+        bool panelFound = false;
+        foreach (Panel p in panels)
+        {
+            switchPanel(p);
+            panelFound = true;
+        }
+        if (!panelFound)
+        {
+            Debug.Log("Panel does not exist in panelList");
+        }
+    }
+
+    public void goToPanel(int panelToSwitchTo)
+    {
+        if (panelToSwitchTo < panels.Count)
+        {
+            switchPanel(panels[panelToSwitchTo]);
+        }
+        else
+        {
+            Debug.Log("Panel does not exist in panelList");
+        }
+    }
+
+    private void switchPanel(Panel panelToSwitchTo, bool addToHistory = true)
+    {
+        bool switchBackground = false;
+        if (panelToSwitchTo.background != thisPanel.background)
+        {
+            switchBackground = true;
+            thisPanel.background.gameObject.SetActive(false);
+        }
+        foreach (Panel p in panels)
+        {
+            if (p == panelToSwitchTo)
+            {
+                //enable correct panel
+                p.enable(true, switchBackground);
+                thisPanel = p;
+                if (addToHistory)
+                {
+                    if (history.Count != 0)
+                    {
+                        if (history[history.Count - 1] != null && history[history.Count - 1] != p)
+                            history.Add(p);
+                    }
+                    else
+                    {
+                        history.Add(p);
+                    }
+                }
+            }
+            else
+            {
+                //disable other panels
+                p.enable(false);
+            }
+
+        }
+    }
+}
+
